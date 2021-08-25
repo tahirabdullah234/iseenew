7263197f216c661136a5e6deaa5ac28379572512
tahirabdullah234 <57060962+tahirabdullah234@users.noreply.github.com>
2021-08-25 13:38:45 +0500

    half dashboard done

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { PatientLogin } from "./components/patientlogin";
 import { PatientRegistration } from "./components/patientregistration";
 import { DoctorRegistration } from "./components/docreg";
 import { DoctorLogin } from "./components/doctorlogin";
+import MainDrawer from "./components/drawer";
+import { PatientDashboard } from "./components/PatientDashboard";
 function App() {
   return (
     <div>
-      <DoctorRegistration />
+      <PatientDashboard />
     </div>
   );
 }
