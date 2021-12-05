77b361fd81938a8802296f020ab5117a789630a2
tahirabdullah234 <57060962+tahirabdullah234@users.noreply.github.com>
2021-12-05 16:35:22 +0500

    error removed

--- src/pages/statesSlice.js
+++ src/pages/statesSlice.js
@@ -1,50 +1,49 @@
 import { createSlice } from "@reduxjs/toolkit";
 // redux to manage states easily
 export const stateSlice = createSlice({
-    name: 'states',
-    initialState: {
-        islogin: false,
-        user: {},
-        token: '',
-        isdoctor: false,
-        name: "",
-        data: null,
-        doctors: null,
-        requesteddocs: [],
-        recieved_requests: null,
-        appointments: null,
-    },
-    setappointments: (state, action) => {
-      state.appointments = action.payload;
-    },
-    login: (state) => {
-      state.islogin = true;
-    },
-    logout: (state) => {
-      state.islogin = false;
-    },
-    settoken: (state, action) => {
-      state.token = action.payload;
-    },
-    setuser: (state, action) => {
-      state.user = action.payload;
-      state.name = action.payload.fname + " " + action.payload.lname;
-    },
-    setdoctortrue: (state) => {
-      state.isdoctor = true;
-    },
-    setdoctorfalse: (state) => {
-      state.isdoctor = false;
-    },
-    setdata: (state, action) => {
-      state.data = action.payload;
-    },
-    setrequesteddoc: (state, action) => {
-      state.requesteddocs = action.payload;
-    },
-    setdoctors: (state, action) => {
-      state.doctors = action.payload;
-    },
+  name: "states",
+  initialState: {
+    islogin: false,
+    user: {},
+    token: "",
+    isdoctor: false,
+    name: "",
+    data: null,
+    doctors: null,
+    requesteddocs: [],
+    recieved_requests: null,
+    appointments: null,
+  },
+  setappointments: (state, action) => {
+    state.appointments = action.payload;
+  },
+  login: (state) => {
+    state.islogin = true;
+  },
+  logout: (state) => {
+    state.islogin = false;
+  },
+  settoken: (state, action) => {
+    state.token = action.payload;
+  },
+  setuser: (state, action) => {
+    state.user = action.payload;
+    state.name = action.payload.fname + " " + action.payload.lname;
+  },
+  setdoctortrue: (state) => {
+    state.isdoctor = true;
+  },
+  setdoctorfalse: (state) => {
+    state.isdoctor = false;
+  },
+  setdata: (state, action) => {
+    state.data = action.payload;
+  },
+  setrequesteddoc: (state, action) => {
+    state.requesteddocs = action.payload;
+  },
+  setdoctors: (state, action) => {
+    state.doctors = action.payload;
   },
 });
 
