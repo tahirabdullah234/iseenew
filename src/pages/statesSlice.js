daa8e40f3f9d99266c92885aa600856b8d81c8cd
tahirabdullah234 <57060962+tahirabdullah234@users.noreply.github.com>
2021-12-05 16:33:09 +0500

    basic

--- src/pages/statesSlice.js
+++ src/pages/statesSlice.js
@@ -1,70 +1,70 @@
 import { createSlice } from "@reduxjs/toolkit";
 // redux to manage states easily
 export const stateSlice = createSlice({
-    name: 'states',
-    initialState: {
-        islogin: false,
-        user: {},
-        token: '',
-        isdoctor: true,
-        name: "",
-        data: null,
-        doctors: null,
-        requesteddocs: [],
-        recieved_requests: null,
-        appointments: null,
+  name: "states",
+  initialState: {
+    islogin: false,
+    user: {},
+    token: "",
+    isdoctor: false,
+    name: "",
+    data: null,
+    doctors: null,
+    requesteddocs: [],
+    recieved_requests: null,
+    appointments: null,
+  },
+  reducers: {
+    setrecivedreq: (state, action) => {
+      state.recieved_requests = action.payload;
     },
-    reducers: {
-        setrecivedreq: (state, action) => {
-            state.recieved_requests = action.payload
-        },
-        setappointments: (state, action) => {
-            state.appointments = action.payload
-        },
-        login: (state) => {
-            state.islogin = true;
-        },
-        logout: (state) => {
-            state.islogin = false;
-        },
-        settoken: (state, action) => {
-            state.token = action.payload;
-        },
-        setuser: (state, action) => {
-            state.user = action.payload;
-            state.name = action.payload.fname + " " + action.payload.lname;
-        },
-        setdoctortrue: (state) => {
-            state.isdoctor = true;
-        },
-        setdoctorfalse: (state) => {
-            state.isdoctor = false;
-        },
-        setdata: (state, action) => {
-            state.data = action.payload
-        },
-        setrequesteddoc: (state, action) => {
-            state.requesteddocs = action.payload
-        },
-        setdoctors: (state, action) => {
-            state.doctors = action.payload
-        }
+    setappointments: (state, action) => {
+      state.appointments = action.payload;
     },
+    login: (state) => {
+      state.islogin = true;
+    },
+    logout: (state) => {
+      state.islogin = false;
+    },
+    settoken: (state, action) => {
+      state.token = action.payload;
+    },
+    setuser: (state, action) => {
+      state.user = action.payload;
+      state.name = action.payload.fname + " " + action.payload.lname;
+    },
+    setdoctortrue: (state) => {
+      state.isdoctor = true;
+    },
+    setdoctorfalse: (state) => {
+      state.isdoctor = false;
+    },
+    setdata: (state, action) => {
+      state.data = action.payload;
+    },
+    setrequesteddoc: (state, action) => {
+      state.requesteddocs = action.payload;
+    },
+    setdoctors: (state, action) => {
+      state.doctors = action.payload;
+    },
+  },
 });
 
 // Action creators are generated for each case reducer function
 export const {
-    login,
-    logout,
-    settoken,
-    setuser,
-    setdoctortrue,
-    setdoctorfalse,
-    setdata,
-    setrequesteddoc,
-    setdoctors,
-    setrecivedreq,
-    setappointments,
-} = stateSlice.actions
+  login,
+  logout,
+  settoken,
+  setuser,
+  setdoctortrue,
+  setdoctorfalse,
+  setdata,
+  setrequesteddoc,
+  setdoctors,
+  setrecivedreq,
+  setappointments,
+} = stateSlice.actions;
 
 export default stateSlice.reducer;
